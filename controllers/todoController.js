016cb56500dceefa3dbd53f7bbf11c81d4d559eb
Mike Zou <zoutianh@grinnell.edu>
2017-07-23 00:48:38 -0500

    successfully completed the tutorial

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,25 +1,54 @@
 var parser = require('body-parser');
+var mongoose = require('mongoose');
+var mongo = require('mongodb').MongoClient;
+
+//Connect to database
+var remoteurl = "mongodb://zoutianh:f2H6OYbk12AYHSGa@cluster0-shard-00-00-uiery.mongodb.net:27017,cluster0-shard-00-01-uiery.mongodb.net:27017,cluster0-shard-00-02-uiery.mongodb.net:27017/Epidemic?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+// mongo.connect(remoteurl, function(err, db) {
+//   if (err) throw err;
+//   db.collection('todos').insert({item: "watch more tutorials"});
+//   console.log('Data Saved');
+//   db.close();
+// });
+mongoose.Promise = global.Promise;
+mongoose.connect(remoteurl);
+
+// Create a schema - this is somewhat like a blueprint for database
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+// capitalize first letter because it's the model - based on Schema
+var Todo = mongoose.model('Mike', todoSchema);
+// var itemOne = Todo({item: 'get tutorials'}).save(function(err) {
+//   if (err) throw err;
+//   console.log('Item saved');
+// });
 
 var urlencodedParser = parser.urlencoded({extended: false});
-
-var data = [{item: 'get milk'},
-{item: 'walk dog'}, {item: 'watch Breaking Bad'}];
 
 module.exports = function(app) {
 
   app.get('/todo', function(req, res) {
-    res.render('todo', {todos: data});
+    //get data from mongodb and pass it to view
+    Todo.find({}, function(err, info) {
+      if (err) throw err;
+      res.render('todo', {todos: info});
+    })
   });
 
   app.post('/todo', urlencodedParser, function(req, res) {
-    data.push(req.body);
-    res.json(data);
+    //get data from view and add it to mongodb
+    var newTodo = Todo(req.body).save(function(err, data) {
+      if (err) throw err;
+      res.json(data);
+    });
   });
 
   app.delete('/todo/:item', function(req, res) {
-    data = data.filter(function(todo) {
-      return todo.item.replace(/ /g, '-') !== req.params.item;
+    //delete the requested item from mongodb
+    Todo.find({item: req.params.item.replace(/\-/g, ' ')}).remove(function(err, data) {
+      if (err) throw err;
+      res.json(data);
     });
-    res.json(data);
   });
 };

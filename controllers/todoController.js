3b5c8ba342262932e2f2524898597e6f74415fab
Mike Zou <zoutianh@grinnell.edu>
2017-07-22 23:29:09 -0500

    created controller and app.js

---
+++ controllers/todoController.js
@@ -0,0 +1,25 @@
+var parser = require('body-parser');
+
+var urlencodedParser = parser.urlencoded({extended: false});
+
+var data = [{item: 'get milk'},
+{item: 'walk dog'}, {item: 'watch Breaking Bad'}];
+
+module.exports = function(app) {
+
+  app.get('/todo', function(req, res) {
+    res.render('todo', {todos: data});
+  });
+
+  app.post('/todo', urlencodedParser, function(req, res) {
+    data.push(req.body);
+    res.json(data);
+  });
+
+  app.delete('/todo/:item', function(req, res) {
+    data = data.filter(function(todo) {
+      return todo.item.replace(/ /g, '-') !== req.params.item;
+    });
+    res.json(data);
+  });
+};
